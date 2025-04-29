0e80cd504e9b3b90c6b0bcc0ee7d7cf5463d8764
ming.wang4 <ming.wang4@envision-digital.com>
2025-04-29 17:07:55 +0800

    springboot

---
+++ src/main/java/com/swang/myservice/MyIgniteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swang.myservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyIgniteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyIgniteServiceApplication.class, args);
+	}
+
+}
